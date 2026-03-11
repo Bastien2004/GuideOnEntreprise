380c8f853552156cfc3af6e4414878c7127c9fe8
Bastien2004 <bastienhecquet2004@gmail.com>
2026-03-11 19:39:13 +0100

    Responsive

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -10,6 +10,9 @@
 
     <script src="https://cdn.tailwindcss.com"></script>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css">
+
+    <link rel="icon" href="{{ asset('images/logo.png') }}" type="logo/png">
+
 </head>
 <body class="bg-[#fcfdfe] text-slate-900 selection:bg-blue-600 selection:text-white">
 
