b13002779480d7c07b5697992b33aa13f8468b4d
Bastien2004 <bastienhecquet2004@gmail.com>
2026-03-11 19:51:41 +0100

    Responsive

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -11,7 +11,7 @@
     <script src="https://cdn.tailwindcss.com"></script>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css">
 
-    <link rel="icon" href="{{ asset('images/logo.png') }}" type="logo/png">
+    <link rel="icon" href="{{ asset('images/logo.png') }}" type="logdocker exec guideon-app ls /var/www/public/css/o/png">
 
 </head>
 <body class="bg-[#fcfdfe] text-slate-900 selection:bg-blue-600 selection:text-white">
